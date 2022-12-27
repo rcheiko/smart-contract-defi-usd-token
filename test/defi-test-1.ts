02ad199db068c8cca2035182477a7589ade402ca
rcheiko <rcheiko@student.42nice.fr>
2022-12-27 10:52:40 +0100

    added some unit testing

--- test/defi-test-1.ts
+++ test/defi-test-1.ts
@@ -3,44 +3,44 @@ import { ethers, upgrades } from "hardhat";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 describe("Defi - token/swap Smart Contract", function () {
   before(async function () {
-    [this.owner, this.fundWallet, this.testWallet] = await ethers.getSigners();
-    this.decimals_token = 10 ** 18;
-    this.decimals_usd = 10 ** 6;
+    [this.owner, this.fundWallet, this.testWallet] = await ethers.getSigners()
+    this.decimals_token = 10 ** 18
+    this.decimals_usd = 10 ** 6
   });
 
   it("Should deploy the smart contract", async function () {
-    console.log("###################");
-    console.log("owner wallet :", this.owner.address);
-    console.log("fund wallet :", this.fundWallet.address);
-    console.log("test wallet :", this.testWallet.address);
-    console.log("###################\n");
+    console.log("###################")
+    console.log("owner wallet :", this.owner.address)
+    console.log("fund wallet :", this.fundWallet.address)
+    console.log("test wallet :", this.testWallet.address)
+    console.log("###################\n")
 
-    this.usdToken = await ethers.getContractFactory("usd");
-    this.usdDeployed = await this.usdToken.deploy();
+    this.usdToken = await ethers.getContractFactory("usd")
+    this.usdDeployed = await this.usdToken.deploy()
 
-    await this.usdDeployed.deployed();
-    console.log("usdDeployed: " + this.usdDeployed.address + "\n");
+    await this.usdDeployed.deployed()
+    console.log("usdDeployed: " + this.usdDeployed.address + "\n")
 
-    this.defiToken = await ethers.getContractFactory("LDG01");
+    this.defiToken = await ethers.getContractFactory("LDG01")
     this.tokenDeployed = await upgrades.deployProxy(this.defiToken, {
       initializer: "initialize",
-    });
+    })
 
     await this.tokenDeployed.deployed();
     console.log("tokenDeployed: " + this.tokenDeployed.address + "\n");
 
-    this.defiSwap = await ethers.getContractFactory("ldgSwap");
+    this.defiSwap = await ethers.getContractFactory("ldgSwap")
     this.swapDeployed = await upgrades.deployProxy(this.defiSwap, {
       initializer: "initialize",
-    });
+    })
 
     await this.swapDeployed.deployed();
-    console.log("swapDeployed: " + this.swapDeployed.address + "\n");
+    console.log("swapDeployed: " + this.swapDeployed.address + "\n")
 
     this.MINT_ROLE = await this.tokenDeployed.MINT_ROLE();
 
@@ -92,14 +92,14 @@ describe("Defi - token/swap Smart Contract", function () {
     // check if the fund wallet received the usd
     const balanceUSDFundWallet = await this.usdDeployed.balanceOf(
       this.fundWallet.address
-    );
+    )
     expect(JSON.parse(balanceUSDFundWallet)).to.equal(1 * this.decimals_usd);
     
     await time.increase(60 * 60 * 24 * 365); // 1 year
 
     const balanceLTY = await this.tokenDeployed.balanceOf(this.testWallet.address)
     expect(JSON.parse(balanceLTY)).to.equal(1.05 * this.decimals_token) // 5 % more for 1 year of staking
-  });
+  })
 
   it("Should approve/withdraw 1 lty for 1 usd with test Wallet", async function () {
     await this.tokenDeployed.connect(this.testWallet).approve(this.swapDeployed.address, BigInt(1 * this.decimals_token))
@@ -113,12 +113,25 @@ describe("Defi - token/swap Smart Contract", function () {
     expect(JSON.parse(balanceUSDFundWallet)).to.equal(0)
 
     const balanceLTYTestWallet = await this.tokenDeployed.balanceOf(this.testWallet.address)
-    console.log(JSON.parse(balanceLTYTestWallet))
-    expect(JSON.parse(balanceLTYTestWallet)).to.below(1 * this.decimals_token)
+    expect(JSON.parse(balanceLTYTestWallet)).to.below(0.06 * this.decimals_token).to.above(0.05 * this.decimals_token)
 
     const balanceUSDTestWallet = await this.usdDeployed.balanceOf(this.testWallet.address)
     expect(JSON.parse(balanceUSDTestWallet)).to.equal(11 * this.decimals_usd)
+  })
 
-    
+  it("Should transfer lty token -> Test wallet to Owner wallet and hold 1 year", async function () {
+
+    await this.tokenDeployed.connect(this.testWallet).transfer(this.owner.address, BigInt(0.05 * this.decimals_token))
+
+    const balanceLTYOwnerWallet1 = await this.tokenDeployed.balanceOf(this.owner.address)
+    expect(JSON.parse(balanceLTYOwnerWallet1)).to.equal(0.05 * this.decimals_token)
+
+    const balanceLTYTestWallet = await this.tokenDeployed.balanceOf(this.testWallet.address)
+    expect(JSON.parse(balanceLTYTestWallet)).to.below(0.01 * this.decimals_token)
+
+    await time.increase(60 * 60 * 24 * 365); // 1 year
+
+    const balanceLTYOwnerWallet2 = await this.tokenDeployed.balanceOf(this.owner.address)
+    expect(JSON.parse(balanceLTYOwnerWallet2)).to.equal(0.0525 * this.decimals_token) // + 5 % of 0.05 is 0.0525
   })
 });
